37e8d5f80e2508de9ff3fc0fdd3c4cef50ed29cd
Ivan Aldinata <ivan.aldinata@gmail.com>
2020-04-21 14:37:00 +0700

    Initial Commit

---
+++ MovComXIB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovComXIB
+//
+//  Created by Ivan on 21/04/20.
+//  Copyright © 2020 ivan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
